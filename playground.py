d9e77a760e76c4eb1c97a884ea53cb13f43a6cef
arif <arif@bitmascot.com>
2021-06-02 12:40:46 +1000

    Requirements

--- playground.py
+++ playground.py
@@ -3,17 +3,11 @@ import torch.nn as nn
 from torchvision import datasets, transforms
 import autoencoder
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5), (0.5))
-    ])
+data = torch.randn(1, 64,1,1,dtype=torch.cfloat)
+print(data.shape)
+model = nn.Sequential(
+     nn.Flatten()
+)
 
-#transform = transforms.ToTensor()
-mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data,
-                                          batch_size=64,
-                                          shuffle=True)
-
-dataiter = iter(data_loader)
-images, labels = dataiter.next()
-print(torch.min(images), torch.max(images))
+output = model(data)
+print(output.shape)
